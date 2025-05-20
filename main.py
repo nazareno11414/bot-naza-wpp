34e6b2c89d3355abb266c04881f213c65b53dfe7
Nazareno Cammarota <54371834+nazareno11414@users.noreply.github.com>
2025-05-19 22:18:44 -0300

    main.py

--- main.py
+++ main.py
@@ -62,5 +62,7 @@ def webhook():
 
     return jsonify({"status": "ok"}), 200
 
+
 if __name__ == "__main__":
-    app.run(port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
